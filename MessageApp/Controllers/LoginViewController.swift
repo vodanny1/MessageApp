9a2eee2aacb808f743f5f96062b0c5a7cf3086b9
Danny Vo <danny.vo@mail.utoronto.ca>
2020-07-04 17:13:12 -0400

    New controller files

--- MessageApp/Controllers/LoginViewController.swift
+++ MessageApp/Controllers/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  MessageApp
 //
 //  Created by Danny Vo on 2020-07-04.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
 
-
 }
-
