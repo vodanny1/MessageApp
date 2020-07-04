4f45d05dca20b0b2b668d7460d71484707527793
Danny Vo <danny.vo@mail.utoronto.ca>
2020-07-04 16:58:13 -0400

    Messaging app using Firebase Database
    Messaging app using Firebase Database

---
+++ MessageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessageApp
+//
+//  Created by Danny Vo on 2020-07-04.
+//  Copyright © 2020 Danny Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
